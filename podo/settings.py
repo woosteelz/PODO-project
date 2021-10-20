585a71255c0716b93a009ec5be972dc27cf39974
woosteelz <woosteelz@naver.com>
2021-10-20 19:15:08 +0900

    ADD requirements.txt HIDE secretkey

--- podo/settings.py
+++ podo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qnnn4h9q8xza+a_knr3^&u65+=pk8l^toi(240yzw&_pfdh8et'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
