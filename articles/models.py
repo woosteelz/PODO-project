404f4ca0485c573836c4ac6199297f3296e8612e
HongJungKim <hong267@naver.com>
2021-10-27 21:24:14 +0900

    test hongjung

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class Board():
+    pass
+
+
+class Article(models.Model):
+    pass
+
+
+class Comment():
+    pass
+
+
+class Image():
+    pass
+
+
+class File():
+    pass
+
+
