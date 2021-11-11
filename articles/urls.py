dac0b5b869abf2f6e31a1b8465e2eb617c84c5ad
HongJungKim <hong267@naver.com>
2021-11-12 00:42:04 +0900

    1112 [Add drag&drop]

--- articles/urls.py
+++ articles/urls.py
@@ -7,6 +7,7 @@ app_name = 'articles'
 urlpatterns = [
     path('<int:workspace_pk>/workspace/<int:category_pk>/category/', views.index_article, name='index_article'),
     path('<int:workspace_pk>/workspace/<int:category_pk>/category/<int:board_pk>/board/create_article/', views.create_article, name='create_article'),
+    path('<int:article_pk>/<int:board_pk>/modify_article_board/', views.modify_article_board, name='modify_article_board'),
     path('<int:article_pk>/detail_article/', views.detail_article, name='detail_article'),
     path('<int:article_pk>/update_article/', views.update_article, name='update_article'),
     path('<int:article_pk>/delete_article/', views.delete_article, name='delete_article'),
