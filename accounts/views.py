c0446af5cae635fcd2d2ebb7a60a14a7dc15303b
khyunchoi <khyunchoi@naver.com>
2021-11-13 00:01:29 +0900

    1113 Update[UI, invitations]

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import logout as auth_logout, update_session_auth_hash
 from django.views.decorators.http import require_POST, require_http_methods
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserChangeForm
 from django.contrib.auth.forms import PasswordChangeForm
+
+from invitations.utils import get_invitation_model
 
 
 # Create your views here.
@@ -21,6 +23,15 @@ def update(request):
     return render(request, 'accounts/update.html', context)
 
 
+@require_POST
+def delete_image(request):
+    user = request.user
+    user.image = None
+    user.save()
+    return redirect('accounts:update')
+
+
+@require_POST
 def delete(request):
     if request.user.is_authenticated:
         request.user.delete()
@@ -49,3 +60,17 @@ def password_change(request):
         'form': form,
     }
     return render(request, 'accounts/password_change.html', context)
+
+
+@require_http_methods(['GET', 'POST'])
+def invitations_send_invite(request):
+    if request.method == 'POST':
+        Invitation = get_invitation_model()
+        email = request.POST.get('email')
+        invite = Invitation.create(email, inviter=request.user)
+        invite.send_invitation(request)
+        return redirect('/workspaces/')
+    else:
+        pass
+    context = {}
+    return render(request, 'accounts/invite.html', context)
