ae7111806b1cc16769c31c850d59ceae31acc0b9
khyunchoi <khyunchoi@naver.com>
2021-11-15 21:09:11 +0900

    1115 Add[UI, invitation member]

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('delete/', views.delete, name='delete'),
     path('logout/', views.logout, name='logout'),
     path('password/change/', views.password_change, name='password_change'),
-    path('invitations/send-invite/', views.invitations_send_invite, name='invitations_send_invite')
+    path('invitations/send-invite/', views.invitations_send_invite, name='invitations_send_invite'),
+    path('invitations/member/<int:workspace_pk>/', views.invitations_member, name='invitations_member'),
 ]
